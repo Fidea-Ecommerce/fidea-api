4096c60bc1ed5fd293950c9014a7a94d9c99314b
nexblu <farras.pramudita@gmail.com>
2024-05-18 08:28:45 +0700

    feat: add new headers

--- main.py
+++ main.py
@@ -47,7 +47,9 @@ app.register_error_handler(400, handle_400)
 async def add_cors_headers(response):
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    response.headers["Access-Control-Allow-Headers"] = (
+        "Content-Type, Authorization, X-Forwarded-For, User-Agent"
+    )
     return response
 
 
